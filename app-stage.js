874729b1165b2e65b4c36dcc284a5fab6a82a172
drinkingtheink <jason.m.harrison@gmail.com>
2022-12-23 19:50:31 -0600

    searching reddit and showing results

--- app-stage.js
+++ app-stage.js
@@ -12,24 +12,57 @@ export class AppStage extends LitElement {
     `;
   }
 
+  _fetchData() {
+    fetch(`https://www.reddit.com/search.json?q=`+ this.searchTerm)
+      .then(response => response.json())
+      .then(response => response["data"].children || [])
+      .then(response => {
+          console.log('Success:', response);
+          this._searchResults = response;
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+      });
+  }
+
   static get properties() {
     return {
       /**
        * @type {object}
        */
-      dataResp: {type: Object | null},
+      _searchResults: {type: Array},
     };
   }
 
   constructor() {
     super();
-    this.dataResp = null;
+    this.searchTerm = '';
+    this._searchResults = [];
   }
 
   render() {
     return html`
       <h2>Let's Make a Call!</h2>
+      <input @input=${this.changeSearchTerm} placeholder="Enter your search term">
+      <button @click=${this._fetchData} part="button">
+        VROOM
+      </button>
+
+      <section class="results-stage">
+      ${this._searchResults.map(
+        (item) => html`
+          <li>
+            <p>${item.data?.title}</p>
+            <p>${item.data?.permalink}</p>
+          </li>`
+      )}
+      </section>
     `;
+  }
+
+  changeSearchTerm(event) {
+    const input = event.target;
+    this.searchTerm = input.value;
   }
 }
 
