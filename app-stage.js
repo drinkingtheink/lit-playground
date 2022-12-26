391c2d495283e782a24ac337f02701b32e79a6b0
drinkingtheink <jason.m.harrison@gmail.com>
2022-12-25 20:54:14 -0600

    split search response into posts created and comments on posts

--- app-stage.js
+++ app-stage.js
@@ -9,16 +9,30 @@ export class AppStage extends LitElement {
         padding: 16px;
         max-width: 800px;
       }
+
+      .results {
+        list-style: none;
+        padding: none;
+        margin: none;
+      }
+
+      .result {
+        border: 1px solid black;
+        padding: 1rem 2rem;
+        margin-bottom: 1rem;
+      }
     `;
   }
 
   _fetchData() {
-    fetch(`https://www.reddit.com/search.json?q=`+ this.searchTerm)
+    fetch(`https://www.reddit.com/user/`+ this.searchTerm + `.json`)
       .then(response => response.json())
-      .then(response => response["data"].children || [])
+      .then(response => response['data'].children || [])
       .then(response => {
           console.log('Success:', response);
           this._searchResults = response;
+          this._commentsOnPosts = response.filter((result) => result.kind == 't1');
+          this._postsCreated = response.filter((result) => result.kind == 't3');
       })
       .catch((error) => {
           console.error('Error:', error);
@@ -27,10 +41,9 @@ export class AppStage extends LitElement {
 
   static get properties() {
     return {
-      /**
-       * @type {object}
-       */
       _searchResults: {type: Array},
+      _commentsOnPosts: {type: Array},
+      _postsCreated: {type: Array},
     };
   }
 
@@ -38,24 +51,41 @@ export class AppStage extends LitElement {
     super();
     this.searchTerm = '';
     this._searchResults = [];
+    this._commentsOnPosts = [];
+    this._postsCreated = [];
   }
 
   render() {
     return html`
-      <h2>Let's Make a Call!</h2>
-      <input @input=${this.changeSearchTerm} placeholder="Enter your search term">
+      <h2>Investigate a User:</h2>
+      <input @input=${this.changeSearchTerm} placeholder="Enter a username to search for">
       <button @click=${this._fetchData} part="button">
         VROOM
       </button>
 
       <section class="results-stage">
-      ${this._searchResults.map(
-        (item) => html`
-          <li>
-            <p>${item.data?.title}</p>
-            <p>${item.data?.permalink}</p>
-          </li>`
-      )}
+        <h2>Posts Created (${this._postsCreated.length}):</h2>
+        <ul class="posts-created results">
+          ${this._postsCreated.map(
+            (item) => html`
+              <li class="result">
+                <h3><a .href=${item.data?.link_permalink}>${item.data?.title || item.data?.link_title}</a></h3>
+                <p>${item.data?.subreddit_name_prefixed}</p>
+              </li>`
+          )}
+        </ul>
+
+        <h2>Comments on Posts (${this._commentsOnPosts.length}):</h2>
+        <ul class="comments-on-posts results">
+          ${this._commentsOnPosts.map(
+            (item) => html`
+              <li class="result">
+                <h3><a .href=${item.data?.link_permalink}>${item.data?.title || item.data?.link_title}</a></h3>
+                <p class="comment-body">${item.data?.body}</p>
+                <p>${item.data?.subreddit_name_prefixed}</p>
+              </li>`
+          )}
+        </ul>
       </section>
     `;
   }
